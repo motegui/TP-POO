0fb4d174d4e665851f30ef6fe5582945eb94c397
motegui <80490650+motegui@users.noreply.github.com>
2022-12-07 13:29:03 -0300

    changes en el front

---
+++ src/frontend/FrontFigures/FrontEllipse.java
@@ -0,0 +1,19 @@
+package frontend.FrontFigures;
+
+import javafx.scene.canvas.GraphicsContext;
+
+public class FrontEllipse extends FrontFigure{
+
+    //super
+
+    @Override
+    public void fill(GraphicsContext gc) {
+
+
+    }
+
+    @Override
+    public void stroke(GraphicsContext gc) {
+
+    }
+}
---
+++ src/frontend/FrontFigures/FrontRectangle.java
@@ -0,0 +1,4 @@
+package frontend.FrontFigures;
+
+public class FrontRectangle {
+}
