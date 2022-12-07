e1ad2e4dd8247eeb0d4dcc46c103118131c0cfad
Nayeth2019 <fedegarcia@itba.edu.ar>
2022-12-07 12:29:44 -0300

    prueba

--- src/frontend/AppLauncher.java
+++ src/frontend/AppLauncher.java
@@ -8,7 +8,7 @@ import javafx.stage.Stage;
 public class AppLauncher extends Application {
 
 	public static void main(String[] args) {
-		launch(args);
+		launch(args);System.out.println("asd");
 	}
 
 	@Override
